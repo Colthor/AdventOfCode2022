16e4f4f3bed0ef3c4eac7cd1af591671f78ff310
MailShop <michael@themailshop.co.uk>
2022-12-22 17:09:28 +0000

    I think the code's parsing and generating properly now, but it's too long for the web interpreter ;_;

--- d13/day13.purs
+++ d13/day13.purs
@@ -10,29 +10,36 @@ import Data.String
 import TryPureScript (render, withConsole)
 
 data NumList
-  = Num Int
-  | Lst (List NumList)
+  = N Int
+  | L (List NumList)
+  
+data Items = I Int (List NumList) (List NumList)
+
+sumPackets :: (List Items) -> Int
+sumPackets Nil = 0
+sumPackets (Cons (I i left right) tail)
+  | -1 == comparePacket left right = i + sumPackets tail
+  | otherwise = sumPackets tail
   
 --Return -1 if left is smaller, 0 if the same, +1 if bigger
-comparePacket :: Partial => (List NumList) -> (List NumList) -> Int
+comparePacket :: (List NumList) -> (List NumList) -> Int
 comparePacket Nil Nil          = 0
 comparePacket (Cons _ _) (Nil) = 1
 comparePacket (Nil) (Cons _ _) = -1
-comparePacket (Cons (Num a) as) (Cons (Num b) bs)
+comparePacket (Cons (N a) as) (Cons (N b) bs)
   | a < b = -1
   | b < a = 1
   | a == b = comparePacket as bs
-comparePacket (Cons (Lst a) as) (Cons (Lst b) bs)
+comparePacket (Cons (L a) as) (Cons (L b) bs)
   | 0 == comparePacket a b = comparePacket as bs
   | otherwise = comparePacket a b
-comparePacket (Cons (Num a) as) (Cons (Lst b) bs) =
-  comparePacket (Cons (Lst (Cons (Num a) Nil)) as) (Cons (Lst b) bs)
-comparePacket (Cons (Lst b) bs) (Cons (Num a) as) =
-  comparePacket (Cons (Lst b) bs) (Cons (Lst (Cons (Num a) Nil)) as)
+comparePacket (Cons (N a) as) (Cons (L b) bs) =
+  comparePacket (Cons (L (Cons (N a) Nil)) as) (Cons (L b) bs)
+comparePacket (Cons (L b) bs) (Cons (N a) as) =
+  comparePacket (Cons (L b) bs) (Cons (L (Cons (N a) Nil)) as)
+comparePacket (Cons (N _) _) (Cons (N _) _) = 0 --I don't know why this is
   
   
-strToPacket :: String -> (List NumList)
-strToPacket a  
-  | uncons a == Nothing = Nil
-  | uncons a == Just { a, tail } = 
-    if "[" == a then
+main :: Effect Unit
+main = render =<< withConsole do
+  logShow( ... )
